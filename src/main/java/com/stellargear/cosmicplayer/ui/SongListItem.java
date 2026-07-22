6771e6b70e9ded19e7e85883e15629252ba5ee11
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2026-07-21 22:50:20 -0500

    style: update SongListItem ui

--- src/main/java/com/stellargear/cosmicplayer/ui/SongListItem.java
+++ src/main/java/com/stellargear/cosmicplayer/ui/SongListItem.java
@@ -1,25 +1,49 @@
 package com.stellargear.cosmicplayer.ui;
 
+import java.io.File;
+
+import com.stellargear.cosmicplayer.utils.Methods;
+
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
 
 public class SongListItem {
 
-    private HBox box;
+    private HBox songTimeBox;
+    private HBox generalBox;
+
+    private VBox controlBox;
+
     private Label songName = new Label("");
     private Label artistName = new Label("");
     private Label songDuration = new Label("");
 
-    public SongListItem (String song, String artist, String duration) {
-        box = new HBox(songName, artistName, songDuration);
+    private final ImageView coverArtBox = new ImageView();
+
+    public SongListItem (File songFile, String song, String artist, String duration, byte[] imageData) {
+        songTimeBox = new HBox(5, songName, songDuration);
+        controlBox = new VBox(songTimeBox, artistName);
+        generalBox = new HBox(4, coverArtBox, controlBox);
+
+        coverArtBox.setFitWidth(40);
+        coverArtBox.setFitHeight(40);
+        coverArtBox.setPreserveRatio(true);
+
+        generalBox.getStyleClass().add("list-item");
+
+        update(songFile, song, artist, duration, imageData);
+    }
+
+    public void update(File songFile, String song, String artist, String duration, byte[] imageData) {
         songName.setText(song);
         artistName.setText(artist);
         songDuration.setText(duration);
-
-        box.getStyleClass().add("list-item");
+        coverArtBox.setImage(Methods.toCachedThumbnail(songFile, imageData, 40));
     }
 
     public HBox getNode () {
-        return box;
+        return generalBox;
     }
 }
